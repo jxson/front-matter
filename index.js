fb1d76143ca3a7e9f881939635d15847ae4be2f0
Jason Campbell <jason@greatergood.cc>
2012-06-22 13:01:09 -0700

    copy front-matter extactor and added pacakge.json

---
+++ index.js
@@ -0,0 +1,25 @@
+var yaml = require('yamlparser')
+;
+
+module.exports = function(data){
+  // http://stackoverflow.com/q/1068308
+  var regex = /^(\s*---([\s\S]+)---\s*)/gi
+    , match = regex.exec(data)
+    , attributes
+    , body
+
+  if (match && match.length > 0){
+    var yamlString = match[2].replace(/^\s+|\s+$/g, '')
+
+    attributes = yaml.eval(yamlString)
+    body = data.replace(match[0], '')
+  } else {
+    attributes = {};
+    body = data;
+  }
+
+  return {
+    attributes: attributes,
+    body: body
+  };
+}
